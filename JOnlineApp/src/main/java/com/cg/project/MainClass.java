a0d6fdb706e13faa6d98bbcf0b2b4043f5c98b44
soumya24-1 <mbsoumya24@gmail.com>
2022-03-24 17:51:28 +0530

    2nd time Commit

--- JOnlineApp/src/main/java/com/cg/project/MainClass.java
+++ JOnlineApp/src/main/java/com/cg/project/MainClass.java
@@ -8,6 +8,8 @@ public class MainClass
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+    	 System.out.println( " Wel Come to JavaOnlineApp " );
+        System.out.println( "This is Java Online App Project" );
+       
     }
 }
